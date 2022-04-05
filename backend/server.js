dbce38c94cf74d70778e2dd0100c952ae43a5d77
Víctor Barreto <victorbarretolins@gmail.com>
2022-04-05 12:02:06 -0300

    created a route for all the goals

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,8 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
+app.get("/api/goals", (request, response) => {
+    response.status(200).json({message : "Get goals"})
+})
+
 app.listen(port, () => console.log(`Server started on port ${port}`))
