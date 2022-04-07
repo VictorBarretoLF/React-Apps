666a786b5f2f1f5e2babe5f200a8e2690403a7d9
Víctor Barreto <victorbarretolins@gmail.com>
2022-04-07 18:15:59 -0300

    hashing user password for authetication

--- backend/controlers/userControler.js
+++ backend/controlers/userControler.js
@@ -1,23 +1,68 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../model/userModel')
+
 // %desc Register new user
 // route POST /api/users
 // @access Public
-const registerUser = (request, response) => {
+const registerUser = asyncHandler( async(request, response) => {
+
+    const { name, email, password} = request.body
+    // check if the form is completed or not
+    if (!name || !email || !password){
+        response.status(400)
+        throw new Error("Please add all fields")
+    }
+    // check if the user exists
+    const userExists = await User.findOne({email})
+    // console.log(userExists)
+    if(userExists){
+        response.status(400)
+        throw new Error("User already exists")
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // Create User
+    const user = await User.create({
+        name,
+        email,
+        password : hashedPassword,
+    })
+
+    // confirm if the user was created or not
+    if (user) {
+        response.status(200).json({
+            _id : user.id,
+            name : user.name,
+            email : user.email,
+        })
+    } else {
+        response.status(400)
+        throw new Error("Invalid user data")
+    }
+
+    // console.log(hashedPassword)
+    // console.log(name, email, password)
     response.json({message : "Register User"})
-}
+})
 
 // %desc Authenticate a user
 // route POST /api/users/login
 // @access Public
-const loginrUser = (request, response) => {
+const loginrUser = asyncHandler( async(request, response) => {
     response.json({message : "Login User"})
-}
+})
 
 // %desc Get user data
 // route GET /api/users/me
 // @access Public
-const getMe = (request, response) => {
+const getMe = asyncHandler( async(request, response) => {
     response.json({message : "Show user data"})
-}
+})
 
 module.exports = {
     registerUser,
