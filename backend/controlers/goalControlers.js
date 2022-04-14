6cb2446ea4d1b2004906a5808a4f237777dad5a4
Víctor Barreto <victorbarretolins@gmail.com>
2022-04-14 16:56:01 -0300

    finished user authorizations and permisions

--- backend/controlers/goalControlers.js
+++ backend/controlers/goalControlers.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler")
 
+const User = require('../model/userModel')
 const Goal = require('../model/goalsModel')
 
 // %desc Get goals
@@ -19,8 +20,10 @@ const setGoal = asyncHandler(async (request, response) => {
         response.status(400)
         throw new Error("Please add a text field")
     }
+    
     const goal = await Goal.create({
         text : request.body.text,
+        user : request.user.id
     })
 
     return response.status(200).json(goal)
@@ -36,6 +39,20 @@ const updateGoal = asyncHandler(async (request, response) => {
     if (!goal){
         response.status(400)
         throw new Error("Goal not found")
+    }
+
+    const user = await User.findById(request.user.id)
+
+    // Check for user
+    if (!user){
+        response.status(401)
+        throw new Error('User not found')
+    }
+
+    // Make sure the logged in user matches the goal user
+    if (goal.user.toString() !== user.id){
+        response.status(401)
+        throw new Error('User not authorized')
     }
 
     const updatedGoal = await Goal.findByIdAndUpdate(request.params.id, request.body, {new : true})
