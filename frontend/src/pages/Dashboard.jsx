0f53713a7a2bbcf9efb8074789f3b29da2a66567
Víctor Barreto <victorbarretolins@gmail.com>
2022-10-18 11:28:38 -0300

    initialzing the Dashboard basic config

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Dashboard(props) {
-    return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
-    );
+  const navigate = useNavigate();
+
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if(!user) {
+        navigate('/login')
+    }
+  }, [user, navigate])
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
 }
 
 export default Dashboard;
