cb6b4af2ff9669bcfd3a2784dc5e9c7e8255bdaf
Víctor Barreto <victorbarretolins@gmail.com>
2022-10-22 17:30:02 -0300

    implementing socket.io on the backend for user connect and disconnect

--- chat-application/backend/index.js
+++ chat-application/backend/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
-const socketio = require("socket.io");
+const { Server } = require("socket.io");
 const http = require("http");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
