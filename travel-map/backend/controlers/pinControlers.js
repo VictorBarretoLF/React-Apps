ca8d956038cc5052a1c76ecdf6baf449bab7c07c
Víctor Barreto <victorbarretolins@gmail.com>
2022-10-25 12:05:36 -0300

    finished the pin controler

--- travel-map/backend/controlers/pinControlers.js
+++ travel-map/backend/controlers/pinControlers.js
@@ -3,12 +3,24 @@ const asyncHandler = require("express-async-handler");
 const Pin = require("../model/pinModel");
 
 // %desc Create a Pin
-// route POST
+// route POST /api/pin
 // @access
 const createPin = asyncHandler(async (request, response) => {
-  console.log('batendo aqui');
+  const { username, title, desc, rating, long, lat } = request.body;
 
-  response.status(200).json(request.body);
+  if (!username || !title || !desc || !rating || !long || !lat) {
+    response.status(400);
+    throw new Error("Please add all fields!");
+  }
+
+  const pin = await Pin.create(request.body);
+
+  if (pin) {
+    return response.status(201).json(pin);
+  }
+
+  response.status(400);
+  throw new Error("Invalid data");
 });
 
 module.exports = {
