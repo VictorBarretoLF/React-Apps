2b1994889e59db4e7c22fd003e8df99faa5ef494
Víctor Barreto <victorbarretolins@gmail.com>
2022-10-27 15:15:37 -0300

    fixed cors error on backend

--- travel-map/backend/index.js
+++ travel-map/backend/index.js
@@ -4,8 +4,11 @@ const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const { errorHandler } = require("./middleware/errorMiddleware");
+const cors = require("cors");
 
 connectDB();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
