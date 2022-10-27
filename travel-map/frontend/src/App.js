715e50b5a19be37f5f7bccd0700f5ff7f37ad6f6
Víctor Barreto <victorbarretolins@gmail.com>
2022-10-26 23:49:30 -0300

    adding mui to the project

--- travel-map/frontend/src/App.js
+++ travel-map/frontend/src/App.js
@@ -1,47 +1,83 @@
-import { Fragment, useCallback, useEffect, useRef, useState } from "react";
-import ReactMapGL, { GeolocateControl, NavigationControl } from "react-map-gl";
-import MapBoxGeocoder from "@mapbox/mapbox-gl-geocoder";
+import { Fragment, useEffect, useRef, useState } from "react";
+import Map, { Marker, NavigationControl, Popup } from "react-map-gl";
+import { Room, Star } from "@mui/icons-material";
 
 import "mapbox-gl/dist/mapbox-gl.css";
-import Geocoder from "./findLocation/Geocoder";
-
+import { Button, CardContent, Typography } from "@mui/material";
 // https://www.youtube.com/watch?v=JyPn_o_UJCM
 // import Mapbox from "react-map-gl/dist/esm/mapbox/mapbox";
 
 function App() {
-  const [viewport, setViewport] = useState({
-    longitude: -34.861,
-    latitude: -7.11532,
-    zoom: 14,
-  });
-
-  const mapRef = useRef();
-  // console.log(navigator.geolocation.getCurrentPosition());
-  // console.log(Geocoder);
-
-  console.log(mapRef);
-
-  useEffect(() => {}, []);
-
   return (
     <Fragment>
-      <ReactMapGL
-        ref={mapRef}
+      <Map
         initialViewState={{
-          longitude: -34.861,
-          latitude: -7.11532,
-          zoom: 14,
+          longitude: -34.7966738,
+          latitude: -7.1487534,
+          zoom: 5,
         }}
-        dragPan={true}
-        mapboxAccessToken={
-          "pk.eyJ1IjoiYmFycmV0b2xpbnMiLCJhIjoiY2w5b2h4YTZ1MDI4eTN2cXBudHpsanphayJ9.KyvZA8IIn_wymorOJ7zKTQ"
-        }
-        style={{ width: 600, height: 400 }}
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+        style={{ width: "100vw", height: "100vh" }}
         mapStyle="mapbox://styles/mapbox/streets-v11"
       >
         <NavigationControl position="top-left" />
-        <Geocoder />
-      </ReactMapGL>
+        <Marker longitude={-34.7966738} latitude={-7.1487534}>
+          <Room
+            style={{
+              fontSize: 50,
+              color: "slate-blue",
+            }}
+          />
+        </Marker>
+        <Popup longitude={-34.7966738} latitude={-7.1487534} anchor="left">
+          <CardContent>
+            <Typography
+              variant="h6"
+              sx={{
+                textDecoration: "underline",
+                color: "tomato",
+              }}
+            >
+              Place
+            </Typography>
+            <Typography
+              sx={{
+                fontSize: 16,
+                margin: "4px 0px",
+                fontWeight: "bold",
+              }}
+            >
+              Farol de Cabo Branco
+            </Typography>
+            <Typography
+              variant="h6"
+              sx={{
+                textDecoration: "underline",
+                color: "tomato",
+              }}
+            >
+              Review
+            </Typography>
+            <Typography
+              sx={{
+                fontSize: 16,
+                margin: "4px 0px",
+              }}
+            >
+              Beautiful place. I like it.
+            </Typography>
+            <Typography
+              variant="h6"
+              sx={{
+                textDecoration: "underline",
+                color: "tomato",
+              }}
+            >
+              Rating
+            </Typography>
+          </CardContent>
+        </Popup>
+      </Map>
     </Fragment>
   );
 }
