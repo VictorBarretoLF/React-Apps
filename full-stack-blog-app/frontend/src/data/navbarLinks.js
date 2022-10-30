226e0f2295df28c6b5a176f8158e97cdb4f83277
Víctor Barreto <victorbarretolins@gmail.com>
2022-10-30 13:05:24 -0300

    added a data folder and navbar links list

---
+++ full-stack-blog-app/frontend/src/data/navbarLinks.js
@@ -0,0 +1,28 @@
+const navbarLinks = [
+  {
+    linkTo: "/?cat=art",
+    title: "ART",
+  },
+  {
+    linkTo: "/?cat=science",
+    title: "SCIENCE",
+  },
+  {
+    linkTo: "/?cat=technology",
+    title: "TECHNOLOGY",
+  },
+  {
+    linkTo: "/?cat=cinema",
+    title: "CINEMA",
+  },
+  {
+    linkTo: "/?cat=design",
+    title: "DESIGN",
+  },
+  {
+    linkTo: "/?cat=food",
+    title: "FOOD",
+  },
+];
+
+export default navbarLinks;
