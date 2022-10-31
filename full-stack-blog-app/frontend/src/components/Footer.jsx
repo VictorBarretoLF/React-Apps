548d0e9a5d421169e88900eaef94d40b966a3974
Víctor Barreto <victorbarretolins@gmail.com>
2022-10-31 11:09:08 -0300

    added styling and content to the footer

--- full-stack-blog-app/frontend/src/components/Footer.jsx
+++ full-stack-blog-app/frontend/src/components/Footer.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+import Logo from "../assets/logo.png";
 
 const Footer = () => {
-    return (
-        <div>
-            footer
-        </div>
-    );
+  return (
+    <footer className="footer">
+      <img src={Logo} alt="logo" />
+      <span>
+        Made with love and <b>React.js</b>.  
+      </span>
+    </footer>
+  );
 };
 
 export default Footer;
