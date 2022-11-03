a1966bef14447df63f97d41ad0fdc59e92b00aaa
Víctor Barreto <victorbarretolins@gmail.com>
2022-11-02 22:39:29 -0300

    creating a database in phpAdmin

--- full-stack-blog-app/backend/index.js
+++ full-stack-blog-app/backend/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+
+
+
 app.use(express.json());
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
