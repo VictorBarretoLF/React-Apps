db2393940ebf83f9c3cbc890ef0797df50f2085c
Víctor Barreto <victorbarretolins@gmail.com>
2022-11-08 18:06:38 -0300

    adicionando uma funcionalidade fake para o login do usuario

--- full-stack-blog-app/frontend/src/pages/Login.jsx
+++ full-stack-blog-app/frontend/src/pages/Login.jsx
@@ -1,15 +1,51 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+  const [err, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const data = await axios.post("/auth/login", inputs);
+      console.log(data)
+      navigate("/");
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
+
   return (
     <div className="auth">
       <h2>Login</h2>
-      <form className="auth__login-form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
+      <form className="auth__login-form" onSubmit={handleSubmit}>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+        />
         <button>Login</button>
-        <p>This is an error!</p>
+        {err && <p>{err}</p>}
         <span>
           Don't you have an account? <Link to="/register">Register</Link>
         </span>
