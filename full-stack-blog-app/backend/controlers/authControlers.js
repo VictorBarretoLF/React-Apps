4d1e3b3d8a56e73d1735f8c8e88ba510330115cb
Víctor Barreto <victorbarretolins@gmail.com>
2022-11-09 04:20:52 -0300

    finished login function for the user

--- full-stack-blog-app/backend/controlers/authControlers.js
+++ full-stack-blog-app/backend/controlers/authControlers.js
@@ -1,13 +1,14 @@
 const asyncHandler = require("express-async-handler");
 const db = require("../config/db");
-var bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // %desc Register user
 // route POST /api/auth/register
 // @access Public
 const register = asyncHandler(async (req, res) => {
   const { email, username, password } = req.body;
-  
+
   if (!email || !username || !password) {
     res.status(400);
     throw new Error("Please add all fields");
@@ -38,7 +39,35 @@ const register = asyncHandler(async (req, res) => {
 // route POST /api/auth/login
 // @access Public
 const login = asyncHandler(async (req, res) => {
-  res.json("user login");
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    res.status(400);
+    return res.json("Please add all fields");
+  }
+
+  const q = "SELECT * FROM users WHERE username = ?";
+  db.query(q, [username], (err, data) => {
+    if (err) return res.json(err);
+    if (data.length === 0) return res.status(404).json("User not found!");
+
+    // Check Password
+    const isPasswordCorrect = bcrypt.compareSync(password, data[0].password);
+
+    if (!isPasswordCorrect)
+      return res.status(400).json("Wrong username or password!");
+
+    const token = jwt.sign({ id: data[0].id }, "jwtkey");
+
+    delete data[0].password;
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(data[0]);
+  });
 });
 
 // %desc Logout user
--- full-stack-blog-app/backend/index.js
+++ full-stack-blog-app/backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const db = require("./config/db");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 5000;
 
@@ -11,6 +12,7 @@ db.connect((err) => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/posts", require("./routes/postsRoutes"));
 app.use("/api/auth", require("./routes/authRoutes"));
