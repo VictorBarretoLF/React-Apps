0663105526c0169b213b054c14749420e75d1748
Víctor Barreto <victorbarretolins@gmail.com>
2022-11-17 18:36:55 -0300

    criando component de lista e card

--- pipefy-clone/src/components/Card/index.js
+++ pipefy-clone/src/components/Card/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+
+import { Container, Label } from "./styles";
 
 const index = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <Container>
+      <header>
+        <Label color="#7159c1" />
+      </header>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis a
+        ipsa.?
+      </p>
+      <img
+        src="https://vendasmegga.loja2.com.br/img/c01bbc2ee9e3f13b1c69213b48dccc4f.jpg"
+        alt="avatar"
+      />
+    </Container>
+  );
 };
 
 export default index;
