82cc0abea8ca46c09be9dc684a1a8e60d94ca03d
Víctor Barreto <victorbarretolins@gmail.com>
2022-11-17 19:30:26 -0300

    finishing styling the lists and using the mokup data

--- pipefy-clone/src/components/Card/index.js
+++ pipefy-clone/src/components/Card/index.js
@@ -2,20 +2,17 @@ import React from "react";
 
 import { Container, Label } from "./styles";
 
-const index = () => {
+const index = ({ data }) => {
   return (
     <Container>
       <header>
-        <Label color="#7159c1" />
+        {data.labels.map((label) => (
+          <Label key={label} color={label} />
+        ))}
       </header>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis a
-        ipsa.?
-      </p>
-      <img
-        src="https://vendasmegga.loja2.com.br/img/c01bbc2ee9e3f13b1c69213b48dccc4f.jpg"
-        alt="avatar"
-      />
+      <p>{data.content}</p>
+
+      {data.user && <img src={data.user} alt="avatar" />}
     </Container>
   );
 };
