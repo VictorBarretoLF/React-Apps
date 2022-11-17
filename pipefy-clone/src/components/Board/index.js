fa1e9dd6ce2eb740c739cb09fe8f56cbda535e5f
Víctor Barreto <victorbarretolins@gmail.com>
2022-11-17 18:25:33 -0300

    criando containers

---
+++ pipefy-clone/src/components/Board/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import List from "../List";
+
+import { Container } from "./styles";
+
+const index = () => {
+  return (
+    <Container>
+      <List />
+      <List />
+      <List />
+      <List />
+    </Container>
+  );
+};
+
+export default index;
---
+++ pipefy-clone/src/components/List/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const index = () => {
+  return (
+    <header>
+      <h2>Tarefas</h2>
+      <button type="button">+</button>
+    </header>
+  );
+};
+
+export default index;
